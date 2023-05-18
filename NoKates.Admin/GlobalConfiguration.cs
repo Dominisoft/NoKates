623b3c481dd879bc41d25bcd63e321cbada86452
Dominic Hemken <dphemken@gmail.com>
2023-05-18 13:19:49 -0500

    fixed runtime error with bindings

---
+++ NoKates.Admin/GlobalConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using NoKates.Common.Infrastructure.Configuration;
+
+namespace NoKates.Admin
+{
+    public class GlobalConfiguration
+    {
+        public static void LoadValuesFromNokates()
+        {
+            IdentityServiceUrl = ConfigurationValues.Values["IdentityServiceUrl"];
+            RootEndpointUrl = ConfigurationValues.Values["BaseUrl"];
+            LogsAndMetricsServiceUrl = ConfigurationValues.Values["LogsAndMetricsServiceUrl"];
+            BaseUrl = ConfigurationValues.Values["BaseUrl"];
+            HostSiteName = ConfigurationValues.Values["HostSiteName"];
+            ConfigurationServiceUrl = ConfigurationValues.Values["ConfigurationServiceUrl"];
+        }
+
+        public static string IdentityServiceUrl { get; set; }
+    
+        public static string RootEndpointUrl { get; set; }
+    public static string LogsAndMetricsServiceUrl { get; set; }
+    public static int ResponseTimeThreshold { get; set; } = 100;
+        public static int ErrorPercentThreshold { get; set; } = 5;
+        public static string BaseUrl { get; set; }
+    public static string HostSiteName { get; set; }
+    public static string ConfigurationServiceUrl { get; set; }
+}
+}
